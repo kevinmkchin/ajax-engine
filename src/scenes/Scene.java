b05b50126ae0bc087490fd430b39b675b6be394c
Kevin Chin <44921110+kevinmkchin@users.noreply.github.com>
2019-03-01 01:23:34 -0800

    set up project for matrices

---
+++ src/scenes/Scene.java
@@ -0,0 +1,9 @@
+package scenes;
+
+public abstract class Scene {
+
+    //create entire levels (i.e. loading .objs and setting their transforms, etc) in scene classes
+
+    //when you switch levels (i.e. switch game state to a different scene) just render stuff inside the scene class
+
+}
